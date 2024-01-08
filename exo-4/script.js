2a4feafaf2f9886ac8dc19840fd5a37ef476e07f
Boubacar Gadio <boubacar.gadio@le-campus-numerique.fr>
2024-01-08 18:43:00 +0100

    4 commit

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,5 +1,20 @@
 // you can write js here
 
-console.log('exo-4');
 
-var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"].slice(1);
+
+var messagePopped = secretMessage.pop() 
+
+var messageAdded = ["to", "program"];
+secretMessage.push(...messageAdded);
+
+secretMessage[secretMessage.indexOf("easily")] = "right";
+
+secretMessage.unshift("Programming")
+secretMessage.splice(secretMessage.indexOf('get'), 5, "know");
+
+var secretString = secretMessage.join(" ");
+
+console.log(secretMessage);
+console.log(secretString);
+console.log('exo-4');
