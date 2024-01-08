3212b3614eebb2d09df952e3677b9e282e62684f
boubacargadio <boubacar.gadio@le-campus-numerique.fr>
2024-01-08 16:39:57 +0100

    third commit

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,27 @@
 // you can write js here
+const myDate = new Date();
+const hourCurrent = myDate.getHours();
+
+const WeekEnd = hourCurrent > 17 && Day >= 5 ;
+const Week = hourCurrent > 9 && Day <= 5;
+
+
+var isTesting = Boolean();
+var jourFictif = prompt("ecrivez un jour ficitf:");
+
+if (isTesting == true, jourFictif){
+    
+    var Day = jourFictif;
+    console.log("Jour", Day);
+
+} else {
+    var Day = myDate.getDay(); 
+}
+
+
+console.log(jourFictif)
+console.log("heur", hourCurrent);
+console.log("Jour", Day);
+console.log("week", Week);
+console.log("weekend", WeekEnd);
 console.log('exo-2');
--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,54 @@
-// you can write js here
-console.log('exo-3');
+const playerInput = prompt("Your choice ( rock, paper, scissors) ?").toLowerCase()
+
+function getPlayerChoice (playerInput) {
+    
+    if (playerInput === 'rock' || playerInput === 'paper' || playerInput ==='scissors') {
+        return playerInput
+    } else {
+        console.log("Entrez le bon nom!");
+    }
+    
+}
+
+function getComputerChoice () {
+    const computerChoice = Math.floor(Math.random() * 3)
+    // console.log(computerChoice)
+    
+    if (computerChoice === 0){
+        return "rock"
+    } else if (computerChoice ===1){
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+
+function findWinner (playerChoice, computerChoice) {
+
+    if(computerChoice === playerChoice){
+        console.log("egalite");
+    }else if (
+        (playerChoice == "rock" && computerChoice == "scissors") ||
+        (playerChoice == "paper" && computerChoice == "rock" ) ||
+        (playerChoice == "scissors" && computerChoice == "paper")   
+        ){
+        console.log(" win")
+    }
+    else{
+        console.log("lost")
+    }        
+   
+}
+
+function playGame (){
+    const uChoice = getPlayerChoice(playerInput)
+    const computerChoice = getComputerChoice()
+    findWinner(uChoice, computerChoice)
+    console.log("choix computer", computerChoice)
+    console.log("choix utilisateur", uChoice)
+}
+
+playGame()
+
+// console.log('exo-3');
