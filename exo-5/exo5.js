9ca4f7a5da308a54f6744722e10d3a0a632c79a2
boubacargadio <boubacar.gadio@le-campus-numerique.fr>
2024-01-09 17:01:07 +0100

    exo 6

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,19 @@
 // you can write js here
 
+let text = "worlueawfvduoid"
+let input = Array.from(text.toUpperCase());
+let vowels = ["U", "E", "I", "A", "O"];
+let resultArray = [];
+
+
+for (i = 0; i < vowels.length; i++) {
+
+    for (j = 0; j < input.length; j++) {
+        if (input[j] === vowels[i]){
+            resultArray.push(input[j]);
+        }
+    }
+}
+
+console.log(resultArray)
 console.log('exo-5');
--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -11,7 +11,49 @@ var joeInfo = {
 // ! Ne faites pas de modification au dessus de cette ligne !
 
 // 1. Afficher le nombre de voitures de Joe
+console.log(joeInfo.cars.length)
 
 // 2. Changer le nombre de salles de bains de Joe : il n'en possède qu'une. Afficher le nouveau nombre de salles de bain.
+joeInfo.bathrooms = 1;
+console.log(joeInfo.bathrooms)
 
 // 3. Joe vient d'acquérir un garage. Afficher cette nouvelle information. 
+joeInfo.garage = true
+console.log(joeInfo)
+
+
+var football = {
+    equipe: "grenoble foot",
+    nombreJoueur: 23,
+    stade: true,
+    trophe: ["ligue2", "coupe de france"],
+}
+
+const team = {
+    players: {
+        firstName: "Pablo",
+        lastName: "Sanchez",
+        age: 11
+    },
+
+    game: {
+        opponent: "Broncos",
+        teamPoints: 42,
+        opponentPoints: 27
+    },
+    
+};
+
+function addPlayer (firstName, lastName, age) {
+    team(this, firstName, lastName, age)
+    console.log('method player')
+}
+
+function match (opponent, teamPoints, opponentPoints){
+    console.log('method match')
+    team(this, opponent, teamPoints, opponentPoints)
+}
+
+const polm = new addPlayer("polm", "pok", 22)
+
+console.log(addPlayer)
